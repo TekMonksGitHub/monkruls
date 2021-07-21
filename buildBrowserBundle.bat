96a85094c771b08481e041396b88048b75c4b391
TekMonksGitHub <rvkapoor@tekmonks.com>
2021-07-22 02:14:53 +0900

    Better build scripts for Browserify.

--- buildBrowserBundle.bat
+++ buildBrowserBundle.bat
@@ -1,15 +1,15 @@
 @echo off
 
 REM Browserify doesn't support dynamic path requires
-xcopy /E/H "%~dp0\lib" "%~dp0\libForBuild"
-sed -i -e "s/\${CONSTANTS.LIBDIR}/./g" | sed -e "s/\${CONSTANTS.CONFDIR}/..\/conf/g" "%~dp0\libForBuild\monkruls.js"
-sed -i -e "s/\${CONSTANTS.LIBDIR}/./g" | sed -e "s/\${CONSTANTS.CONFDIR}/..\/conf/g" "%~dp0\libForBuild\rulsCoreFunctions.js"
-sed -i -e "s/\${CONSTANTS.LIBDIR}/./g" | sed -e "s/\${CONSTANTS.CONFDIR}/..\/conf/g" "%~dp0\libForBuild\webWrapper.js"
+xcopy /E/H/Y/Q "%~dp0\lib" "%~dp0\libForBuild\"
+type "%~dp0\lib\monkruls.js" | sed -e "s/\${CONSTANTS.LIBDIR}/./g" | sed -e "s/\${CONSTANTS.CONFDIR}/..\/conf/g" > "%~dp0\libForBuild\monkruls.js"
+type "%~dp0\lib\rulsCoreFunctions.js" | sed -e "s/\${CONSTANTS.LIBDIR}/./g" | sed -e "s/\${CONSTANTS.CONFDIR}/..\/conf/g" > "%~dp0\libForBuild\rulsCoreFunctions.js"
+type "%~dp0\lib\webWrapper.js" | sed -e "s/\${CONSTANTS.LIBDIR}/./g" | sed -e "s/\${CONSTANTS.CONFDIR}/..\/conf/g" > "%~dp0\libForBuild\webWrapper.js"
 
 REM Build now
 npx browserify -r "%~dp0\libForBuild\webWrapper.js":webRuls | npx uglifyjs > "%~dp0\lib\monkrulsBrowserified.min.js"
 
 REM Cleanup
-rmdir /s "%~dp0\libForBuild"
+rmdir /S /Q "%~dp0\libForBuild"
 
 echo Done.
--- buildBrowserBundle.sh
+++ buildBrowserBundle.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+# Browserify doesn't support dynamic path requires
+cp -dpr "$SCRIPTPATH/lib" "$SCRIPTPATH/libForBuild"
+cat "$SCRIPTPATH/lib/monkruls.js" | sed -e 's/\${CONSTANTS.LIBDIR}/./g' | sed -e 's/\${CONSTANTS.CONFDIR}/..\/conf/g' > "$SCRIPTPATH/libForBuild/monkruls.js"
+cat "$SCRIPTPATH/lib/rulsCoreFunctions.js" | sed -e 's/\${CONSTANTS.LIBDIR}/./g' | sed -e 's/\${CONSTANTS.CONFDIR}/..\/conf/g' > "$SCRIPTPATH/libForBuild/rulsCoreFunctions.js"
+cat "$SCRIPTPATH/lib/webWrapper.js" | sed -e 's/\${CONSTANTS.LIBDIR}/./g' | sed -e 's/\${CONSTANTS.CONFDIR}/..\/conf/g' > "$SCRIPTPATH/libForBuild/webWrapper.js"
+
+# Build now
 npx browserify -r "$SCRIPTPATH/lib/webWrapper.js":webRuls | npx uglifyjs > "$SCRIPTPATH/lib/monkrulsBrowserified.min.js"
+
+# Cleanup
+rm -rf "$SCRIPTPATH/libForBuild"
+
 echo Done.
